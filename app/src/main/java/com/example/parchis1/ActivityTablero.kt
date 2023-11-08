992d9014e599f3769fe55b7eb412fbdec1e4fe6f
MrCrilopz <mr.crilopz@gmail.com>
2023-11-07 21:15:49 -0500

    Tablero base

---
+++ app/src/main/java/com/example/parchis1/ActivityTablero.kt
@@ -0,0 +1,11 @@
+package com.example.parchis1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityTablero : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tablero)
+    }
+}
