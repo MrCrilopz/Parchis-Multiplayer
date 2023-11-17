ad23aebb5f1181eebb4d987eb79802870d6c5913
ZharikRojas <zrhojazz@gmail.com>
2023-11-16 19:24:49 -0500

    agregando en activity tablero

--- app/src/main/java/com/example/parchis1/ActivityTablero.kt
+++ app/src/main/java/com/example/parchis1/ActivityTablero.kt
@@ -2,10 +2,19 @@ package com.example.parchis1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.parchis1.databinding.ActivityCrearPartidaBinding
+import com.example.parchis1.databinding.ActivityTableroBinding
+import com.google.firebase.FirebaseApp
 
 class ActivityTablero : AppCompatActivity() {
+    private lateinit var binding: ActivityTableroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tablero)
+        binding = ActivityTableroBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        FirebaseApp.initializeApp(this);
+
     }
 }
